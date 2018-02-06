18ae778af69031ca1394588e4ff08ef34668d6bf
ASUS <ASUS@10.89.209.133>
2018-02-06 16:59:48 +0800

    Added Lab1Activity.java

--- src/labs/comp3111/ust/hk/Lab1Activity.java
+++ src/labs/comp3111/ust/hk/Lab1Activity.java
@@ -2,29 +2,4 @@ package labs.comp3111.ust.hk;
 
 public class Lab1Activity {
 
-	public static void main (String[] args) {
-		
-		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		int sum = 0;
-		
-		for(int i=0; i<arr.length; i++) {
-			sum += arr[i];
-		}
-		
-		int max=arr[0];
-		int min=arr[0];
-		for(int i=0; i< arr.length ; i++) {
-			if (arr[i]>max) {
-				max=arr[i];
-			}
-			if (arr[i]<min) {
-				min=arr[i];
-			}
-		}
-		
-		System.out.println(String.format("Sum of the number is %d", sum));
-		System.out.print(String.format("Min = %d", min));
-		System.out.print(String.format("; Max = %d", max));
-		
-	}
 }
